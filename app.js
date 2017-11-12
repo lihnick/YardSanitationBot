4935e905d62d717d95c7d7f6bfac080749a7601e
lihnick <hli109@hawk.iit.edu>
2017-11-12 04:57:33 +0000

    trap error in if statement need to debug

--- app.js
+++ app.js
@@ -50,19 +50,28 @@ app.post('/webhook', (req, res) => {
       // will only ever contain one event, so we get index 0
 
       // Gets the body of the webhook event
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender PSID: ' + sender_psid);
+      let webhook_event, sender_psid;
+      if (entry.messaging && entry.messaging[0]) {
+        webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+        // Get the sender PSID
+        sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
+      }
+      else {
+        console.error("Undefined Entry");
+      }
       
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
-      if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message);        
-      } else if (webhook_event.postback) {
-        handlePostback(sender_psid, webhook_event.postback);
+      if (webhook_event) {
+        if (webhook_event.message) {
+          handleMessage(sender_psid, webhook_event.message);        
+        } else if (webhook_event.postback) {
+          handlePostback(sender_psid, webhook_event.postback);
+        }
+      } else {
+       console.error("Undefined Webhook");
       }
     });
 
